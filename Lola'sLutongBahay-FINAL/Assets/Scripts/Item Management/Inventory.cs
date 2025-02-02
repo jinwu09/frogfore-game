d7ecaf5db56504a78b672fcb071dd121b4c18fde
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2025-02-03 00:21:58 +0800

    inventory fiiix

--- Lola'sLutongBahay-FINAL/Assets/Scripts/Item Management/Inventory.cs
+++ Lola'sLutongBahay-FINAL/Assets/Scripts/Item Management/Inventory.cs
@@ -1,35 +1,79 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
     public static Inventory instance;
 
     private List<ItemManager> itemsList;
+
+    private string filePath = "/player-inventory.json";
+    private IDataService dataService = new JsonDataService();
+    private bool encryptedEnabled;
+    private long savetime;
+    private long loadtimme;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        instance = this;
+        InstanceInventory(false);
         itemsList = new List<ItemManager>();
     }
+    void InstanceInventory(bool createNew)
+    {
+        if (createNew == true) { instance = new Inventory(); return; }
+        if (instance == null) { instance = this; return; }
+    }
+    public void Save_SerializeJson()
+    {
+        long start_time = DateTime.Now.Ticks;
+        if (dataService.SaveData(filePath, itemsList, encryptedEnabled))
+        {
+            savetime = DateTime.Now.Ticks - start_time;
+            Debug.Log($"Inventory saved ({savetime / 1000f})");
+        }
+        else
+        {
+            Debug.LogError("Could not save file! show something on the UI about it!");
+        }
+    }
+    public void Load()
+    {
+        long start_time = DateTime.Now.Ticks;
+        try
+        {
+            List<ItemManager> data = dataService.LoadData<List<ItemManager>>(filePath, encryptedEnabled);
+            loadtimme = DateTime.Now.Ticks - start_time;
+            Debug.Log($"Inventory loaded ({loadtimme / 1000f})");
+            itemsList = data;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Inventory Could not read file!");
 
+        }
+    }
     public void AddItems(ItemManager item)
     {
-        if(item.isStackable)
+        if (item.isStackable)
         {
             bool itemAlreadyInInventory = false;
 
-            foreach(ItemManager itemInInventory in itemsList)
+            foreach (ItemManager itemInInventory in itemsList)
             {
-                if(itemInInventory.itemName == item.itemName)
+                if (itemInInventory.itemName == item.itemName)
                 {
                     itemInInventory.amount += item.amount;
                     itemAlreadyInInventory = true;
                 }
             }
-            
-            if(!itemAlreadyInInventory)
+
+            if (!itemAlreadyInInventory)
             {
                 itemsList.Add(item);
             }
@@ -38,24 +82,24 @@ public class Inventory : MonoBehaviour
         {
             itemsList.Add(item);
         }
-        
+
     }
     public void RemoveItem(ItemManager item)
     {
-        if(item.isStackable)
+        if (item.isStackable)
         {
             ItemManager inventoryItem = null;
 
-            foreach(ItemManager itemInInventory in itemsList)
+            foreach (ItemManager itemInInventory in itemsList)
             {
-                if(itemInInventory.itemName == item.itemName)
+                if (itemInInventory.itemName == item.itemName)
                 {
                     itemInInventory.amount--;
                     inventoryItem = itemInInventory;
                 }
             }
 
-            if(inventoryItem != null & inventoryItem.amount <= 0)
+            if (inventoryItem != null & inventoryItem.amount <= 0)
             {
                 itemsList.Remove(inventoryItem);
             }
--- Lola'sLutongBahay-FINAL/Assets/Scripts/UI/Menus/MenuManager.cs
+++ Lola'sLutongBahay-FINAL/Assets/Scripts/UI/Menus/MenuManager.cs
@@ -9,7 +9,7 @@ public class MenuManager : MonoBehaviour
     [SerializeField] Image imageToFade;
     public GameObject menu;
     [SerializeField] GameObject rpgui;
-    
+
     public static MenuManager instance;
 
     private PlayerStats[] playerStats;
@@ -31,9 +31,9 @@ public class MenuManager : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            if(menu.activeInHierarchy)
+            if (menu.activeInHierarchy)
             {
                 menu.SetActive(false);
                 rpgui.SetActive(true);
@@ -42,6 +42,7 @@ public class MenuManager : MonoBehaviour
             else
             {
                 UpdateStats();
+                UpdateItemsInventory();
                 menu.SetActive(true);
                 rpgui.SetActive(false);
                 GameManager.instance.gameMenuOpened = true;
@@ -53,52 +54,52 @@ public class MenuManager : MonoBehaviour
     {
         playerStats = GameManager.instance.GetPlayerStats();
 
-            characterPanel[0].SetActive(true);
+        characterPanel[0].SetActive(true);
 
-            nameText[0].text = playerStats[0].playerName;
-            lvlText[0].text = "Level: " + playerStats[0].playerLevel;
-            currentXPText[0].text = ": " + playerStats[0].currentXp;
+        nameText[0].text = playerStats[0].playerName;
+        lvlText[0].text = "Level: " + playerStats[0].playerLevel;
+        currentXPText[0].text = ": " + playerStats[0].currentXp;
 
-            xpText[0].text = playerStats[0].currentXp.ToString() + playerStats[0].xpForNextLevel[playerStats[0].playerLevel];
-            xpSlider[0].maxValue = playerStats[0].xpForNextLevel[playerStats[0].playerLevel];
-            xpSlider[0].value = playerStats[0].currentXp;
+        xpText[0].text = playerStats[0].currentXp.ToString() + playerStats[0].xpForNextLevel[playerStats[0].playerLevel];
+        xpSlider[0].maxValue = playerStats[0].xpForNextLevel[playerStats[0].playerLevel];
+        xpSlider[0].value = playerStats[0].currentXp;
     }
 
     public void UpdateItemsInventory()
-{
-    // Clear the item slots container
-    foreach (Transform itemSlot in itemSlotContainerParent)
     {
-        Destroy(itemSlot.gameObject);
-    }
+        // Clear the item slots container
+        foreach (Transform itemSlot in itemSlotContainerParent)
+        {
+            Destroy(itemSlot.gameObject);
+        }
 
-    // Create new item slots and assign sprites
-    foreach (ItemManager item in Inventory.instance.GetItemsList())
+        // Create new item slots and assign sprites
+        foreach (ItemManager item in Inventory.instance.GetItemsList())
+        {
+            RectTransform itemSlot = Instantiate(itemSlotContainer, itemSlotContainerParent).GetComponent<RectTransform>();
+
+            Image itemsImage = itemSlot.Find("Items Image").GetComponent<Image>();
+            itemsImage.sprite = item.itemsImage;
+
+            TextMeshProUGUI itemsAmountText = itemSlot.Find("Amount Text").GetComponent<TextMeshProUGUI>();
+            if (item.amount > 1)
+                itemsAmountText.text = item.amount.ToString();
+            else
+                itemsAmountText.text = "";
+
+            itemSlot.GetComponent<ItemButton>().itemOnButton = item;
+        }
+    }
+    public void DiscardItem()
     {
-        RectTransform itemSlot = Instantiate(itemSlotContainer, itemSlotContainerParent).GetComponent<RectTransform>();
-
-        Image itemsImage = itemSlot.Find("Items Image").GetComponent<Image>();
-        itemsImage.sprite = item.itemsImage;
-
-        TextMeshProUGUI itemsAmountText = itemSlot.Find("Amount Text").GetComponent<TextMeshProUGUI>();
-        if (item.amount > 1)
-            itemsAmountText.text = item.amount.ToString();
-        else
-            itemsAmountText.text = "";
-
-        itemSlot.GetComponent<ItemButton>().itemOnButton = item;
+        print(activeItem.itemName);
+        Inventory.instance.RemoveItem(activeItem);
     }
-}
-public void DiscardItem()
-{
-    print(activeItem.itemName);
-    Inventory.instance.RemoveItem(activeItem);
-}
 
     // Update is called once per frame
     public void FadeImage()
     {
         imageToFade.GetComponent<Animator>().SetTrigger("Start Fading");
-        
+
     }
 }
