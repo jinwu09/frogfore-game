56d76d25892f542f9a656cab0574558d96d853b4
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2025-02-03 13:44:03 +0800

    loading press to continue

--- Lola'sLutongBahay-FINAL/Assets/Scripts/UI/Menus/MainMenuManager.cs
+++ Lola'sLutongBahay-FINAL/Assets/Scripts/UI/Menus/MainMenuManager.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Net.NetworkInformation;
 
 public class MainMenuManager : MonoBehaviour
 {
     public GameObject loadingScreen;
     public Slider slider;
 
-    public void LoadLevel (int sceneToLoad)
+    public void LoadLevel(int sceneToLoad)
     {
         StartCoroutine(LoadAsynchronously(sceneToLoad));
     }
@@ -17,14 +18,24 @@ public class MainMenuManager : MonoBehaviour
     IEnumerator LoadAsynchronously(int sceneToLoad)
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneToLoad);
-
+        operation.allowSceneActivation = false;
         loadingScreen.SetActive(true);
 
         while (!operation.isDone)
         {
-            float progress = Mathf.Clamp01(operation.progress / .9f);
 
-            slider.value = progress;
+
+            slider.value = Mathf.Clamp01(operation.progress / .9f);
+
+            if (operation.progress >= 0.9f)
+            {
+                loadingScreen.transform.GetChild(3).gameObject.SetActive(true);
+                loadingScreen.transform.GetChild(1).gameObject.SetActive(false);
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    operation.allowSceneActivation = true;
+                }
+            }
 
             yield return null;
         }
